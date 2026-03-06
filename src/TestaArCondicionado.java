87785d5bee9c73df2921581ca5d9092b52d24800
unknown <laramorerariberorr5665@gmail.com>
2026-03-05 22:46:55 -0300

    exercicio

---
+++ src/TestaArCondicionado.java
@@ -0,0 +1,28 @@
+public class TestaArCondicionado {
+
+    public static void main(String[] args) {
+
+        // Instanciando objeto
+        ArCondicionado ar = new ArCondicionado("LG", "Dual Inverter", 22);
+
+        // Tentando temperaturas inválida e válida
+        ar.ajustarTemperatura(10);
+        ar.ajustarTemperatura(25);
+
+        // Imprimindo dados
+        System.out.println(ar);
+
+        // Tentando modo turbo desligado
+        ar.modoTurbo();
+
+        // Ligando o aparelho
+        ar.ligar();
+
+        // Ativando modo turbo
+        ar.modoTurbo();
+
+        // Estado final
+        System.out.println("\nEstado final do aparelho:");
+        System.out.println(ar);
+    }
+}
